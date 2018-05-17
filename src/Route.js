20a01f2bf93179cd9eb6d44cbaa6b20c4b26f221
Tim Baker <tbakerx@gmail.com>
2018-05-16 22:17:54 -0700

    Created the layout for the main page and set it as the initial page

--- src/Route.js
+++ src/Route.js
@@ -4,6 +4,7 @@ import {Router, Stack, Scene} from 'react-native-router-flux';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Splash from './pages/Splash';
+import Home from './pages/Home';
 
 export default class Route extends Component<{}> {
   render(){
@@ -11,7 +12,8 @@ export default class Route extends Component<{}> {
       <Router>
         <Stack key="root" hideNavBar={true}>
           <Scene key='splash' component={Splash} title='Splash' />
-          <Scene key='login' component={Login} title='Login' initial={true}/>
+          <Scene key='home' component={Home} title='Home' initial={true} />
+          <Scene key='login' component={Login} title='Login'/>
           <Scene key='signup' component={SignUp} title='SignUp'/>
         </Stack>
       </Router>
---
+++ src/pages/Home.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+
+
+export default class Home extends Component<{}> {
+  
+    signUp(){
+        Actions.signup()
+      }
+    Login(){
+    Actions.login()
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+            <View style={styles.topBar}><Text style={styles.headerText}>GearSwap</Text></View>
+            <View style={styles.main}><Text style={styles.headerText}>Main items Grid Scrollview</Text></View>
+            <View style={styles.bottomBar}>
+            <TouchableOpacity onPress={this.signUp}><Text style={styles.signUpButton}>Sign Up</Text></TouchableOpacity>
+            <TouchableOpacity onPress={this.Login}><Text style={styles.signUpButton}>Login</Text></TouchableOpacity>
+            </View>  
+        </View>
+      );
+    }
+} 
+  
+
+  const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      alignItems: 'stretch',
+      justifyContent: 'center'
+    },
+    topBar:{
+        flex: 1,
+        backgroundColor: '#0d47a1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    main:{
+        flex: 10,
+        backgroundColor: '#bc8600',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bottomBar:{
+        flex: 1,
+        backgroundColor: 'green',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    headerText:{
+        fontSize: 30,
+        fontWeight: '500',
+        color: '#ddd'
+    },
+    signUpButton: {
+        color: '#ccc',
+        fontSize:16,
+        fontWeight: '500'
+      }
+  });
